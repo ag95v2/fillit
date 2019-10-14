452a630298f849518984825c4c72f49ef274b274
Boil Gian <bgian@il-k6.21-school.ru>
2019-10-14 10:48:56 +0300

    First commit. Figuring out how to  read input

---
+++ hardcode_shapes.c
@@ -0,0 +1,13 @@
+
+/*
+**	Hardcoded shapes of all figures as positions from topleft corner of 
+**	4 * 4 square.
+*/
+
+#define T0 {1, 5, 6, 9}
+#define L0 {1, 2, 5, 9}
+#define L1 {1, 2, 6, 10}
+#define SQ {1, 2, 5, 6}
+#define I0 {1, 2, 3, 4}
+#define Z0 {1, 2, 6, 7}
+#define Z1 {2, 3, 6, 7}
