b33c3c2ac8c1f0fafe3face333fd2ff5c988c8ab
anton <goloviznin1995@yandex.ru>
2019-10-15 06:11:38 +0300

    Done hardcoding and rotating shapes

---
+++ Makefile
@@ -0,0 +1,6 @@
+
+HARDCODE = "hardcode_shapes"
+
+
+all:
+	gcc -g -Wall -Wextra -Werror $(HARDCODE).c -I libft/includes -L libft -lft -o $(HARDCODE)
--- hardcode_shapes.c
+++ hardcode_shapes.c
@@ -4,10 +4,120 @@
 **	4 * 4 square.
 */
 
-#define T0 {1, 5, 6, 9}
-#define L0 {1, 2, 5, 9}
-#define L1 {1, 2, 6, 10}
-#define SQ {1, 2, 5, 6}
-#define I0 {1, 2, 3, 4}
-#define Z0 {1, 2, 6, 7}
-#define Z1 {2, 3, 6, 7}
+#define T0 {1 - 1, 5 - 1, 6 - 1, 9 - 1}
+#define L0 {1 - 1, 2 - 1, 5 - 1, 9 - 1}
+#define L1 {1 - 1, 2 - 1, 6 - 1, 10 - 1}
+#define SQ {1 - 1, 2 - 1, 5 - 1, 6 - 1}
+#define I0 {1 - 1, 2 - 1, 3 - 1, 4 - 1}
+#define Z0 {1 - 1, 2 - 1, 6 - 1, 7 - 1}
+#define Z1 {2 - 1, 3 - 1, 5 - 1, 6 - 1}
+
+#define ALL_SHAPES {T0, L0, L1, SQ, I0, Z0, Z1}
+#define N_SHAPES 7
+
+#include "libft.h"
+
+#define SQ_SIDE 4
+#define FIG_SPACE 4
+
+#define	EMPY '.'
+#define	FULL '#'
+
+int	isin(int number, int *array, int len)
+{
+	int	i;
+	
+	i = 0;
+	while (i < len)
+	{
+		if (array[i] == number)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void	print_figure(int *start)
+{
+	int	i;
+	
+	i = 0;
+	while (i < SQ_SIDE * SQ_SIDE)
+	{
+		ft_putchar(isin(i, start, SQ_SIDE) ? FULL : EMPY);
+		if (i % (SQ_SIDE) == SQ_SIDE - 1)
+			ft_putchar('\n');
+		i++;
+	}
+}
+
+/*
+** Axis can be either 
+** 0: horisontal 
+** 1: vertical
+** 2: "main diagonal"
+** In-place operation, array will be modified.
+*/
+void	reflect_shape(int *shape, int axis)
+{
+	int i;
+	int	row;
+	int	col;
+
+	i = 0;
+	while (i < FIG_SPACE) 
+	{
+		col = shape[i] % SQ_SIDE;
+		row = shape[i] / SQ_SIDE;
+		if (axis == 0)
+			shape[i] = (SQ_SIDE - row - 1) * SQ_SIDE + col;
+		if (axis == 1)
+			shape[i] = shape[i] - col + SQ_SIDE - col - 1;
+		if (axis == 2)
+			shape[i] = col * SQ_SIDE + row;
+		i++;
+	}
+}
+
+/*
+**	In-place rotation of tetramino
+**	direction:
+**  0 = clockwise;
+**  1 = counterclockwise;
+*/
+
+void	rotate_shape(int *shape, int direction)
+{
+	reflect_shape(shape, direction ? 1 : 0);
+	reflect_shape(shape, 2);
+}
+
+int	main()
+{
+	int	shapes[N_SHAPES][FIG_SPACE] = ALL_SHAPES;
+	int	i;
+
+	i = 0;
+	while (i < N_SHAPES)
+	{
+		ft_putnbr(i);
+		ft_putstr(":\n");
+		print_figure(shapes[i]);
+		ft_putchar('\n');
+		rotate_shape(shapes[i], 0);
+
+		print_figure(shapes[i]);
+		ft_putchar('\n');
+		rotate_shape(shapes[i], 0);
+
+		print_figure(shapes[i]);
+		ft_putchar('\n');
+		rotate_shape(shapes[i], 0);
+
+		print_figure(shapes[i]);
+		ft_putchar('\n');
+
+		i++;
+	}
+	return (0);
+}
